5d7b5e0c7a18a722208fd11ff16b2bc6a21b1332
Jens Reimann <ctron@dentrassi.de>
2024-02-12 13:59:49 +0100

    feat: add checked for HtmlInputElement

--- src/optimistic/html_input_element.rs
+++ src/optimistic/html_input_element.rs
@@ -1,7 +1,25 @@
+use web_sys::wasm_bindgen::JsCast;
+
 /// Support working with [`web_sys::HtmlInputElement`]
 pub trait OptimisticHtmlInputElement {
     /// Get the [`web_sys::HtmlFormElement`] of an input.
     fn form(&self) -> Option<web_sys::HtmlFormElement>;
+
+    /// Get the [`web_sys::HtmlInputElement::checked`] state of an input.
+    fn checked(&self) -> bool;
+}
+
+impl OptimisticHtmlInputElement for web_sys::Node {
+    fn form(&self) -> Option<web_sys::HtmlFormElement> {
+        self.dyn_ref::<web_sys::HtmlInputElement>()
+            .and_then(|input| input.form())
+    }
+
+    fn checked(&self) -> bool {
+        self.dyn_ref::<web_sys::HtmlInputElement>()
+            .map(|input| input.checked())
+            .unwrap_or_default()
+    }
 }
 
 #[cfg(feature = "yew")]
@@ -9,5 +27,11 @@ impl OptimisticHtmlInputElement for yew::prelude::NodeRef {
     fn form(&self) -> Option<web_sys::HtmlFormElement> {
         self.cast::<web_sys::HtmlInputElement>()
             .and_then(|input| input.form())
+    }
+
+    fn checked(&self) -> bool {
+        self.cast::<web_sys::HtmlInputElement>()
+            .map(|input| input.checked())
+            .unwrap_or_default()
     }
 }
